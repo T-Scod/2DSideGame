07ff7de3319f50c238988b7cb97e0dc680588e1d
Afridi Rahim <36467697+MaapleWaafles@users.noreply.github.com>
2020-04-03 00:51:26 +1100

    - Prototyping Rage Skill

--- 2DSideGame/Assets/Scripts/SkillTree/Skills/RageSkill.cs
+++ 2DSideGame/Assets/Scripts/SkillTree/Skills/RageSkill.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Skill Tree/Passive Skills/Damage Boost")]
-public class DamageBoost : BaseSkill
+[CreateAssetMenu(menuName = "Skill Tree/Active Skills/Raging Fury")]
+public class RageSkill : BaseSkill
 {
+
     [Header("--Skill Init--")]
     public string skillName;
     public string skillDescription;
@@ -17,9 +18,15 @@ public class DamageBoost : BaseSkill
     public string skillIcon;
 
     [Header("--Skill Mechanics--")]
+    public KeyCode activeKey;
     public int boostAmount;
+    public int debuffAmount;
+    private float duration;
+    public float maxDuration;
 
     private int originalDamage;
+    private int originalHealth;
+    private float originalSpeed;
     private GameObject skillBoxObj;
     private GameObject skillIconObj;
     private TempPlayer playerObj;
@@ -36,8 +43,14 @@ public class DamageBoost : BaseSkill
         if (skillBoxObj != null) { skillBoxObj.SetActive(false); }
     }
 
-    #region Skill Intialisation
-    public override void Update() {  }
+    #region Skill Initialisation
+    public override void Update()
+    {
+        if (Input.GetKeyDown(activeKey))
+        {
+
+        }
+    }
 
     public override int GetCost() { return skillCost; }
 
@@ -47,19 +60,17 @@ public class DamageBoost : BaseSkill
 
     public override string GetSkillType() { return skillType; }
 
-    public override string GetRequirements() { return skillRequirement;  }
+    public override string GetRequirements() { return skillRequirement; }
 
     public override GameObject GetSkillBox() { return skillBoxObj; }
 
-    // Players damage is boosted
-    public override void OnEquip() { playerObj.damage += boostAmount; }
+    public override void OnEquip() {  }
 
-    // Players damage is reverted
-    public override void OnUnEquip() { playerObj.damage = originalDamage; }
+    public override void OnUnEquip() { }
 
     public override GameObject GetSkillIcon() { return skillIconObj; }
 
-    public override bool UnlockedSkill() { return isUnlocked; }
+    public override bool UnlockedSkill() { return isUnlocked;  }
 
     public override void MetRequirements()
     {
